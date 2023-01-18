c03e54dcee7c83af2d809433ff5fbc3b6dad01bb
leofracca <leonardofraccaroli1999@gmail.com>
2023-01-18 21:31:34 +0100

    Added initial implementation of A-Priori algorithm

---
+++ Scripts/apriori.py
@@ -0,0 +1,77 @@
+from pymongo import MongoClient
+
+from itertools import combinations
+
+client = MongoClient('mongodb://localhost:27017')
+db = client.TravelReviews
+collection = db.reviews
+
+def apriori(scores, support = 2):
+    # First pass
+
+    # Translate items to numbers
+    # Create a dictionary with the items and their corresponding number
+    items = {}
+    index = 0
+    for s in scores:
+        for i in s:
+            if i not in items:
+                items[i] = index
+                index += 1
+
+    # List of counts for each item
+    # The i-th element of the list is the count of the i-th item
+    counts = [0] * len(items)
+
+    # Count the number of times each item appears
+    for s in scores:
+        for i in s:
+            counts[items[i]] += 1
+
+    # Print the frequent items
+    # for i in items:
+    #     if counts[items[i]] >= support:
+    #         print(i, counts[items[i]])
+
+    # Remove the infrequent items
+    items = {i: items[i] for i in items if counts[items[i]] >= support}
+    counts = [counts[i] for i in items.values()]
+
+    # Second pass
+
+    # Create a list of all possible pairs of items
+    pairs = list(combinations(items.keys(), 2))
+    # print(pairs)
+
+    # List of counts for each pair
+    # The i-th element of the list is the count of the i-th pair
+    counts_pairs = [0] * len(pairs)
+
+    # Count the number of times each pair appears
+    for s in scores:
+        for p in pairs:
+            if p[0] in s and p[1] in s:
+                counts_pairs[pairs.index(p)] += 1
+
+    # Print the frequent pairs
+    for p in pairs:
+        if counts_pairs[pairs.index(p)] >= support:
+            print(p, counts_pairs[pairs.index(p)])
+
+
+# Retrieve all the documents
+documents = collection.find({})
+# Get the scores of each user
+scores = []
+for d in documents:
+    # print(d['scores'])
+    scores.append(d['good_scores'])
+
+# apriori([
+#     ['A', 'B', 'C'],
+#     ['B', 'C', 'D'],
+#     ['A', 'C', 'D'],
+#     ['A', 'B', 'C', 'D'],
+#     ['B', 'D']
+# ])
+apriori(scores)
