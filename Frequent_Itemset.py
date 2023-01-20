3af139581024b65f83e352e8a34dbfe58a8e8199
Albio-dev <alberto.carli_01@studenti.univr.it>
2023-01-20 13:33:47 +0100

    Switched to spark framework

--- Frequent_Itemset.py
+++ Frequent_Itemset.py
@@ -1,15 +1,30 @@
 from pymongo import MongoClient
 from math import floor
+from pyspark.sql import SparkSession
 
-client = MongoClient('mongodb://localhost:27017')
-db = client.TravelReviews
-collection = db.reviews
+db_addr = '127.0.0.1'
+
+spark = SparkSession.builder.config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
+    .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/TravelReviews.reviews") \
+    .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/TravelReviews.reviews") \
+    .getOrCreate()
+#print(spark)
+data = spark.read.format("mongo").load()
+
+print("\n\n")
+#df = spark.read.format("mongo").option("uri", f"mongodb://{db_addr}/TravelReviews.reviews").load()
+
+#client = MongoClient('mongodb://localhost:27017')
+#db = client.TravelReviews
+#collection = db.reviews
 
 
 epsilon = .5
 p = .1
 
-data_len = (collection.count_documents({}))
+
+
+data_len = (data.count())
 basket_len = floor(data_len * p)
 basket_epsilon = epsilon * p
 
@@ -22,7 +37,8 @@ random.shuffle(indexes)
 
 indexes = ([indexes[i*basket_len:(i+1)*basket_len] for i in range(floor(len(indexes)/basket_len))])
 print(indexes[0])
-list(collection.find({"_id":1}, {"$in" : indexes[0][0]}))
+print(list(spark.find({"_id": {"$in" : indexes[0]}})))
 
 
-    
+
+'''
--- SON.py
+++ SON.py
@@ -1,8 +1,29 @@
-from pymongo import MongoClient
+#from pymongo import MongoClient
+from math import floor
 
 class SON:
 
-    def __init__(self):
-        pass
+    def __init__(self, database, p, s):
+        self.db = database
+        self.p = p
+
+        total_len = database.count_documents({})
+
+        self.basket_size = floor(total_len * p)
+        self.basket_s = s * p
+
+    def get_data(self, i):
+
+        interval = list(range(i*self.basket_size, \
+            (i+1)*self.basket_size))
+
+        return list(self.db.find({"_id": {"$in" : interval}}))
+
+    def candidate_fi(self):
+
+        # Split batches
+        # Apriori on batch
+        # Emit fi
+
 
     
