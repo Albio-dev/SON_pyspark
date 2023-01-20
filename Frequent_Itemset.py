f3b3403d1b8422d8deb8946ad7df317f48256def
leofracca <leonardofraccaroli1999@gmail.com>
2023-01-20 17:09:55 +0100

    Trying to understand how to properly use Spark :P

--- Frequent_Itemset.py
+++ Frequent_Itemset.py
@@ -1,17 +1,23 @@
 from pymongo import MongoClient
 from math import floor
 from pyspark.sql import SparkSession
+import SON
 
 db_addr = '127.0.0.1'
 
 spark = SparkSession.builder.config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
     .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/TravelReviews.reviews") \
     .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/TravelReviews.reviews") \
+    .config("spark.mongodb.input.partitioner", "MongoPaginateByCountPartitioner") \
+    .config("spark.mongodb.input.partitionerOptions.partitionKey", "_id") \
+    .config("spark.mongodb.input.partitionerOptions.numberOfPartitions", "10") \
     .getOrCreate()
 #print(spark)
 data = spark.read.format("mongo").load()
 
 print("\n\n")
+print(data)
+print(spark.conf.get("spark.mongodb.input.partitionerOptions.numberOfPartitions"))
 #df = spark.read.format("mongo").option("uri", f"mongodb://{db_addr}/TravelReviews.reviews").load()
 
 #client = MongoClient('mongodb://localhost:27017')
@@ -22,23 +28,23 @@ print("\n\n")
 epsilon = .5
 p = .1
 
+print(data.rdd.getNumPartitions())
 
+son = SON.SON(data, 10, 100)
+son.candidate_frequent_itemsets()
+# son = data.rdd.mapPartitions(lambda x: print(type(x))).collect()
 
-data_len = (data.count())
-basket_len = floor(data_len * p)
-basket_epsilon = epsilon * p
+# data_len = (data.count())
+# basket_len = floor(data_len * p)
+# basket_epsilon = epsilon * p
 
-print((basket_len))
-print(basket_epsilon)
+# print((basket_len))
+# print(basket_epsilon)
 
-import random
-indexes = list(range(data_len))
-random.shuffle(indexes)
+# import random
+# indexes = list(range(data_len))
+# random.shuffle(indexes)
 
-indexes = ([indexes[i*basket_len:(i+1)*basket_len] for i in range(floor(len(indexes)/basket_len))])
-print(indexes[0])
-print(list(spark.find({"_id": {"$in" : indexes[0]}})))
-
-
-
-'''
+# indexes = ([indexes[i*basket_len:(i+1)*basket_len] for i in range(floor(len(indexes)/basket_len))])
+# print(indexes[0])
+# print(list(spark.find({"_id": {"$in" : indexes[0]}})))
--- SON.py
+++ SON.py
@@ -1,29 +1,72 @@
 #from pymongo import MongoClient
 from math import floor
+from itertools import combinations
 
 class SON:
 
-    def __init__(self, database, p, s):
-        self.db = database
-        self.p = p
+    def __init__(self, data, partitions, support):
+        self.data = data
+        self.partitions = partitions
+        self.support = support
+        self.basket_support = self.support / self.partitions
 
-        total_len = database.count_documents({})
 
-        self.basket_size = floor(total_len * p)
-        self.basket_s = s * p
-
-    def get_data(self, i):
-
-        interval = list(range(i*self.basket_size, \
-            (i+1)*self.basket_size))
-
-        return list(self.db.find({"_id": {"$in" : interval}}))
-
-    def candidate_fi(self):
-
-        # Split batches
+    def candidate_frequent_itemsets(self):
         # Apriori on batch
+        x = self.data.rdd.mapPartitions(lambda x: [i.good_scores for i in x]) \
+            .mapPartitions(apriori).collect()
+        # x = self.data.rdd.mapPartitions(self.apriori).collect()#.reduceByKey(lambda x, y: x + y).filter(lambda x: x[1] >= self.basket_support).collect()
+        print(x)
         # Emit fi
 
 
-    
+def apriori(basket):
+    # Translate items to numbers
+    # Create a dictionary with the items and their corresponding number
+    items = {}
+    index = 0
+    # List of counts for each item
+    # The i-th element of the list is the count of the i-th item
+    counts = []
+    for s in basket:
+        for i in s:
+            if i not in items:
+                items[i] = index
+                index += 1
+                counts.append(0)
+            counts[items[i]] += 1
+
+    # Print the frequent items
+    # for i in items:
+    #     if counts[items[i]] >= support:
+    #         print(i, counts[items[i]])
+
+    # Remove the infrequent items
+    items = {i: items[i] for i in items if counts[items[i]] >= 2}
+    counts = [counts[i] for i in items.values()]
+
+    # Second pass
+
+    # Create a list of all possible pairs of items
+    # TODO: is there a better way to do this (without creating all the possible pairs)?
+    pairs = list(combinations(items.keys(), 2))
+    # print(pairs)
+
+    # List of counts for each pair
+    # The i-th element of the list is the count of the i-th pair
+    counts_pairs = [0] * len(pairs)
+    # print(items)
+
+    # Count the number of times each pair appears
+    for s in basket:
+        for p in pairs:
+            if p[0] in s and p[1] in s:
+                counts_pairs[pairs.index(p)] += 1
+
+    # Print the frequent pairs
+    # for p in pairs:
+    #     if counts_pairs[pairs.index(p)] >= 2:
+    #         print(p, counts_pairs[pairs.index(p)])
+
+
+    return [(p, counts_pairs[pairs.index(p)]) for p in pairs if counts_pairs[pairs.index(p)] >= 2]
