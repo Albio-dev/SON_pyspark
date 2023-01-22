2f26c337b2aa178de2a760a2f93d6cd404e5633a
Albio-dev <alberto.carli_01@studenti.univr.it>
2023-01-22 17:36:36 +0100

    Implemented Apriori in SON first map

--- Frequent_Itemset.py
+++ Frequent_Itemset.py
@@ -1,6 +1,9 @@
-from pymongo import MongoClient
+#from pymongo import MongoClient
 from math import floor
 from pyspark.sql import SparkSession
+
+from itertools import combinations
+
 import SON
 
 db_addr = '127.0.0.1'
@@ -18,33 +21,15 @@ data = spark.read.format("mongo").load()
 print("\n\n")
 print(data)
 print(spark.conf.get("spark.mongodb.input.partitionerOptions.numberOfPartitions"))
-#df = spark.read.format("mongo").option("uri", f"mongodb://{db_addr}/TravelReviews.reviews").load()
 
-#client = MongoClient('mongodb://localhost:27017')
-#db = client.TravelReviews
-#collection = db.reviews
 
 
 epsilon = .5
 p = .1
 
 print(data.rdd.getNumPartitions())
+data = data.coalesce(10)
+print(data.rdd.getNumPartitions())
 
 son = SON.SON(data, 10, 100)
 son.candidate_frequent_itemsets()
-# son = data.rdd.mapPartitions(lambda x: print(type(x))).collect()
-
-# data_len = (data.count())
-# basket_len = floor(data_len * p)
-# basket_epsilon = epsilon * p
-
-# print((basket_len))
-# print(basket_epsilon)
-
-# import random
-# indexes = list(range(data_len))
-# random.shuffle(indexes)
-
-# indexes = ([indexes[i*basket_len:(i+1)*basket_len] for i in range(floor(len(indexes)/basket_len))])
-# print(indexes[0])
-# print(list(spark.find({"_id": {"$in" : indexes[0]}})))
--- SON.py
+++ SON.py
@@ -13,12 +13,95 @@ class SON:
 
     def candidate_frequent_itemsets(self):
         # Apriori on batch
-        x = self.data.rdd.mapPartitions(lambda x: [i.good_scores for i in x]) \
-            .mapPartitions(apriori).collect()
+        #x = self.data.rdd.mapPartitions(lambda x: [i.good_scores for i in x]) \
+        #    .mapPartitions(apriori).collect()
         # x = self.data.rdd.mapPartitions(self.apriori).collect()#.reduceByKey(lambda x, y: x + y).filter(lambda x: x[1] >= self.basket_support).collect()
-        print(x)
+        #print(x)
         # Emit fi
 
+        basket_support = self.basket_support
+
+        # Clean up and extract items in every partition
+        baskets = self.data.rdd.mapPartitions(lambda x: [j.good_scores for j in x], preservesPartitioning = True) \
+            .mapPartitions(lambda x: [list(set([k for j in x for k in j])), x], True)
+        
+        # Count and extract items appearances in each basket
+        frequent_itemsets_singlets = baskets.mapPartitions(count_frequencies, True) \
+            .filter(lambda x: x[1] >= basket_support) \
+            .keys() 
+
+        # Save first pass of apriori
+        frequent_itemsets = frequent_itemsets_singlets.glom().collect()
+        # Save singlets for future expansions
+        frequent_items = frequent_itemsets
+
+        # Prepare baskets for next round of counting by stripping them of items and adding partition index
+        #baskets = baskets.mapPartitionsWithIndex(lambda i, x: [i, x[1]])
+        baskets = baskets.mapPartitions(lambda x: x[1])
+
+        # Prepare new itemsets as FI x FI
+        #new_candidates = frequent_itemsets_singlets.mapPartitionsWithIndex(lambda i, x: (i, list(combinations(x, 2))))
+        new_candidates = frequent_itemsets_singlets.mapPartitions(lambda x: list(combinations(x, 2)))
+
+        # Combine structures per partition and clean of indexes
+        #new_structure = new_candidates.zip(baskets).mapPartitions(lambda x: list(x)[1], True)
+        new_structure = new_candidates.glom().zip(baskets.glom()).mapPartitions(lambda x: list(x)[0])
+        # Again, count
+        new_frequent_itemsets = new_structure.mapPartitions(count_frequencies, True) \
+            .filter(lambda x: x[1] >= basket_support) \
+            .keys() 
+
+        while not new_frequent_itemsets.isEmpty():
+            frequent_items = [i + j for i, j in zip(frequent_items, new_frequent_itemsets.glom().collect())]
+            
+
+            # Generate new candidate itemsets starting from frequent ones
+            new_candidate_itemsets = new_frequent_itemsets.glom() \
+                .zip(frequent_itemsets_singlets.glom()) \
+                .glom() \
+                .mapPartitions(lambda x: list(x)[0][0], True) \
+                .mapPartitions(lambda x: [j + (k, ) if isinstance(k, str) else j + k for j in list(x)[0] for k in list(x)[1] if k not in j], True) \
+                .mapPartitions(lambda x: [tuple(j) for j in {frozenset(i) for i in x}])
+                #.mapPartitions(lambda x: [list(j) + [k] for j in list(x)[0] for k in list(x)[1] if k not in j])
+
+            new_structure = new_candidate_itemsets.glom().zip(baskets.glom()).mapPartitions(lambda x: list(x)[0])
+
+            new_frequent_itemsets = new_structure.mapPartitions(count_frequencies, True) \
+                .filter(lambda x: x[1] >= basket_support) \
+                .keys()
+
+        print(frequent_items)
+
+def count_frequencies(data_chunk):
+    itemsets = data_chunk[0]
+    data = data_chunk[1]
+    values = []        
+    #print(f'In itemset {itemsets}')
+    for i in itemsets:
+        #print(f'Item: {i}')
+        count = 0        
+        for j in data:
+            #print(f'basket: {j}')
+            if type(i) not in [list, tuple, set]:
+                if i in j:
+                    count += 1
+            else:
+                if all(items in j for items in i):
+                    count += 1
+        values.append((i, count))
+    #print(f'{values}\n\n\n')
+    return values
+
+'''
+baskets.mapPartitions(count_frequencies) \
+    .filter(lambda x: x[1] >= basket_support) \
+    .keys() \
+    .foreachPartition(lambda x: print(f"{x}\n"))
+  
+baskets.mapPartitions(count_frequencies).collect()
+'''
+
+#baskets.mapPartitions(lambda x: Counter(i for j in x[1] for i in j).items()).collect()
 
 def apriori(basket):
     # Translate items to numbers
