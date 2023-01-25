366283a13d477e78241212384d2e40d400fc4dc7
Albio-dev <alberto.carli_01@studenti.univr.it>
2023-01-25 19:40:43 +0100

    Refactoring

--- Frequent_Itemset.py
+++ Frequent_Itemset.py
@@ -9,25 +9,32 @@ import SON
 db_addr = '127.0.0.1'
 forcePartitions = 10
 
-spark = SparkSession.builder.config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
-    .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/TravelReviews.reviews") \
-    .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/TravelReviews.reviews") \
+spark = (SparkSession.builder
+    .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
+    .config("spark.mongodb.input.uri", f"mongodb://{db_addr}/TravelReviews.reviews") \
+    .config("spark.mongodb.output.uri", f"mongodb://{db_addr}/TravelReviews.reviews") \
     .config("spark.mongodb.input.partitioner", "MongoPaginateByCountPartitioner") \
     .config("spark.mongodb.input.partitionerOptions.partitionKey", "_id") \
     .config("spark.mongodb.input.partitionerOptions.numberOfPartitions", "10") \
     .getOrCreate()
-data = spark.read.format("mongo").load()
+    )
+input_data = spark.read.format("mongo").load()
 
 print("\n\n")
 print(f'Settings partition value:\t\t{spark.conf.get("spark.mongodb.input.partitionerOptions.numberOfPartitions")}')
-print(f'Automatically partitioned value: \t{data.rdd.getNumPartitions()}')
-data = data.coalesce(forcePartitions)
-print(f'Partitions after forcing {forcePartitions}: \t\t{data.rdd.getNumPartitions()}')
+print(f'Automatically partitioned value: \t{input_data.rdd.getNumPartitions()}')
+input_data = input_data.coalesce(forcePartitions)
+print(f'Partitions after forcing {forcePartitions}: \t\t{input_data.rdd.getNumPartitions()}')
+
+data = input_data.rdd.mapPartitions(lambda x: [j.good_scores for j in x])
 
 # Preprocessing: trasformare gli item in numeri
 #items = {i: items[i] for i in items if counts[items[i]] >= support}
 
-epsilon = .5
+
+
+
+epsilon = .85
 
 son = SON.SON(data, epsilon)
 son.candidate_frequent_itemsets()
--- SON.py
+++ SON.py
@@ -5,79 +5,74 @@ from itertools import combinations
 class SON:
 
     def __init__(self, data, support):
-        # Input data as pyspark Dataframe
+        # Input data as pyspark rdd
         self.data = data
         # Reading the number of partitions to properly size everything
-        self.partitions = data.rdd.getNumPartitions()
-        # Counting how many items are in a chunk (TODO: useless?)
-        self.basket_size = data.count()/self.partitions
+        self.partitions = data.getNumPartitions()
         # Reading input support
         self.support = support
         # Scaling support for chunks
-        self.basket_support = self.support / self.partitions
+        self.chunk_support = self.support / self.partitions
 
 
 
     def candidate_frequent_itemsets(self):
 
         # Extract basket support from class (spark doesent like instance attributes)
-        basket_support = self.basket_support
-        # Calculating exactly partitions sizes
-        basket_sizes = [len(i) for i in self.data.rdd.glom().collect()]
+        chunk_support = self.chunk_support
+        data_size = self.data.count()
+        # Clean up and extract items in every partition
+        baskets = self.data
 
         # Debug print
-        print(f'{self.partitions} partizioni\n{basket_support} supporto richiesto in ogni basket\n{self.basket_size} elementi in ogni basket\nChunks sizes: {basket_sizes}')
+        print(f'{self.partitions} partizioni\n{chunk_support} supporto richiesto in ogni basket')#\nChunks sizes: {basket_sizes}')
 
-        # Clean up and extract items in every partition
-        baskets = self.data.rdd.mapPartitions(lambda x: [j.good_scores for j in x], preservesPartitioning = True) # Extract column from every row in partition
-
-        # Extract frequent itemsets from every partition
+        # Extract frequent itemsets from every partition (mapreduce 1)
         candidate_frequent_itemsets = (baskets
-            .mapPartitions(lambda x: apriori2(x, basket_support))   # Applying apriori algorithm on every partition
-            .map(lambda x: (x, 1))                                  # Form key-value shape emitting (itemset, 1)
-            .groupByKey()                                           # Group every itemset
-            .map(lambda x: x[0])                                    # Keep only itemsets
+            .mapPartitions(lambda x: apriori(list(x), chunk_support))   # Applying apriori algorithm on every partition
+            .map(lambda x: (x, 1))                                      # Form key-value shape emitting (itemset, 1)
+            .groupByKey()                                               # Group every itemset
+            .map(lambda x: x[0])                                        # Keep only itemsets
             )
 
-
+        # Check for false positive/false negative (mapreduce 2)
         support = self.support
         frequent_itemsets = (candidate_frequent_itemsets
-            .coalesce(1)                                # Set candidates as a single partition
-            .glom()                                     # Group partition elements together
-            .cartesian(baskets.glom())                  # Distribute to every basket partition
-            .mapPartitions(lambda x: list(x)[0])        # Remove tuple TODO: rendere non necessario
-            .mapPartitions(count_frequencies2)          # Count candidates absolute frequencies for every batch
-            .reduceByKey(lambda x, y: x + y)            # Sum absolute frequencies for every candidate itemset
-            .filter(lambda x: x[1] / 980 >= support)    # Filter out candidates with less than required support
+            .repartition(1)                                             # Set candidates as a single partition
+            .glom()                                                     # Group partition elements together
+            .cartesian(baskets.glom())                                  # Distribute to every basket partition
+            .mapPartitions(lambda x : count_frequencies(list(x)[0]))    # Count candidates absolute frequencies for every batch
+            .reduceByKey(lambda x, y: x + y)                            # Sum absolute frequencies for every candidate itemset
+            .filter(lambda x: x[1] / data_size >= support)              # Filter out candidates with less than required support
             )
 
-        fi = frequent_itemsets.glom().collect()
+        fi = frequent_itemsets.collect()
+        out_fi = []
+
         print(fi)
 
-        # Tenere solo i superset
-        '''frequent_itemsets = new_frequent_itemsets + \
-            [(i, ) if isinstance(i, str) else i \
-            for i in frequent_itemsets \
-                if all( \
-                    [not set([i]).issubset(k) if isinstance(i, str) else not set(i).issubset(k) for k in new_frequent_itemsets] \
-                    )]'''
+        # Keep only larger frequent itemsets
+        for i in sorted([(i[0], ) if isinstance(i[0], str) else i[0] for i in fi], key=len, reverse=True): 
+            if all([not set(i).issubset(set(j)) for j in out_fi]):
+                out_fi.append(i)
+
+        print(out_fi)
 
 
 
 
-
-def apriori2(basket, basket_e):
-    # Basket is passed as an iterator, so we convert it in list
-    basket = list(basket)
+# Apriori algorithm. Requires data as list and accepted support
+def apriori(data, support):
     # Get actual batch size
-    basket_size = len(basket)
+    basket_size = len(data)
+    # Prepare output structure
     frequent_itemsets = []
     # Extract item list
-    items = list(set([k for j in basket for k in j]))
+    items = list(set([k for j in data for k in j]))
     # Use function to get items frequencies in batch
-    temp = count_frequencies2([items, basket])
+    temp = count_frequencies((items, data))
     # Filter only frequent ones and put singlets in tuples
-    frequent_items = [i[0] for i in temp if i[1]/basket_size >= basket_e]
+    frequent_items = [i[0] for i in temp if i[1]/basket_size >= support]
     # Check if any is produced
     new_frequent_itemsets = frequent_items
     while new_frequent_itemsets != []:
@@ -95,13 +90,15 @@ def apriori2(basket, basket_e):
         # Filter out repeated elements (not considering order)
         new_candidate_itemsets = [tuple(j) for j in {frozenset(i) for i in new_candidate_itemsets}]        
         # Count occurrences of the new itemset
-        temp = count_frequencies2([new_candidate_itemsets, basket])
+        temp = count_frequencies((new_candidate_itemsets, data))
         # Filter out non-frequent itemsets
-        new_frequent_itemsets = [i[0] for i in temp if i[1]/basket_size >= basket_e]
+        new_frequent_itemsets = [i[0] for i in temp if i[1]/basket_size >= support]
         
     return frequent_itemsets
         
-def count_frequencies2(data_chunk):
+# Utility function to count occurrences of items in the first list inside elements of the second list
+# e.g.: ( [itemsets], [baskets] )
+def count_frequencies(data_chunk):
     # Itemsets to check
     itemsets = data_chunk[0]
     # Batch to count in itemsets in
