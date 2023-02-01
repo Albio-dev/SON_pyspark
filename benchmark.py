020fdc287dd884a0c1e53104d20a84ceb18e4101
Albio-dev <alberto.carli_01@studenti.univr.it>
2023-02-01 10:30:35 +0100

    Added logging lines

--- benchmark.py
+++ benchmark.py
@@ -59,7 +59,9 @@ def benchmark(dataset, support = 0.5, partitions = None, logging = True, partiti
     # Load spark session with specified parameters
     # selectedDataset: dataset to use
     # forcePartitions: how many partitions to use. None for automatic
+    benchmark_logger.info(f'Started loading DB data...')
     data = Frequent_Itemset.loadspark(selectedDataset='benchmark', forcePartitions=partitions, logger=logger, partition_size=partition_size, samples_per_partition=samples_per_partition)
+    benchmark_logger.info(f'Data loaded.')
     # Run and time SON
     start_time = time.time()
     SON_result = Frequent_Itemset.execute_SON(data, support, logger)
@@ -68,7 +70,9 @@ def benchmark(dataset, support = 0.5, partitions = None, logging = True, partiti
     
     #spark = SparkContext(appName='benchmark')
     spark = data.context
+    benchmark_logger.info(f'Started loading LOCAL data...')
     data = spark.parallelize(dataset, partitions)
+    benchmark_logger.info(f'Data loaded.')
     start_time = time.time()
     SON_result = Frequent_Itemset.execute_SON(data, support, logger)
     benchmark_logger.info(f'Local SON execution time: {time.time() - start_time}s')
