bfc0580603682f32082aa302e057184b43477bfe
Albio-dev <alberto.carli_01@studenti.univr.it>
2023-02-27 19:19:09 +0100

    Set up gridsearch

--- gridsearch.py
+++ gridsearch.py
@@ -3,11 +3,11 @@ import benchmark
 # Set the variables to use for the benchmark. None for automatic
 
 # The different percentages of the dataset to use
-data_sizes = [.01, .05, .1, .15, .2, .25, .3, .5, .8, 1]
+data_sizes = [.01, .05, .1, .15, .2, .25, .3, .5]
 # How many partitions to force
 partitions = [None]
 # The support to use
-supports = [.1]
+supports = [.1, .2, .5]
 
 
 # Run the gridsearch with the given lists of parameters
