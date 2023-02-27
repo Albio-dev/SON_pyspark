694689012814ee20b8ca9b07f85397abb511e07f
Albio-dev <alberto.carli_01@studenti.univr.it>
2023-02-27 19:15:12 +0100

    Added database section to report

--- gridsearch.py
+++ gridsearch.py
@@ -5,13 +5,10 @@ import benchmark
 # The different percentages of the dataset to use
 data_sizes = [.01, .05, .1, .15, .2, .25, .3, .5, .8, 1]
 # How many partitions to force
-partitions = [8]
+partitions = [None]
 # The support to use
-supports = [.9]
+supports = [.1]
 
-# Parameters for the mongodb connector
-partition_sizes = [None]
-samples_per_partition = [None]
 
 # Run the gridsearch with the given lists of parameters
-benchmark.gridsearch(data_sizes=data_sizes, partitions=partitions, supports = supports, partition_sizes=partition_sizes, samples_per_partition=samples_per_partition)
+benchmark.gridsearch(data_sizes=data_sizes, partitions=partitions, supports = supports)
