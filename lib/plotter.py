8d68f1735fb819aec7962f80f24bd8c18b28df4f
Albio-dev <alberto.carli_01@studenti.univr.it>
2023-03-24 16:25:19 +0100

    Changes to plotter function for better readability

--- lib/plotter.py
+++ lib/plotter.py
@@ -3,7 +3,7 @@ def plot(ax, dataset, title):
     frequencies = [x[1] for x in dataset]
     x_indexes = list(range(0,len(dataset)))
     x_labels = [x[0] for x in dataset]
-    ax.set_xticks(x_indexes, x_labels)
+    ax.set_xticks(x_indexes, x_labels, rotation=-15)
     ax.plot(frequencies)
     ax.set_title(title)
     ax.set_xlabel('Frequent itemsets')
